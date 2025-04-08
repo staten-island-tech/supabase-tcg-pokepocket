63634948b7a703b416cc9e98af3136d407dc5cb0
NotLeo27 <158211664+NotLeo27@users.noreply.github.com>
2025-04-08 12:32:30 -0400

    E

--- app/src/components/LoginPage.vue
+++ app/src/components/LoginPage.vue
@@ -1,17 +1,17 @@
 <template>
-    <div>
-        <form @submit.prevent>
-            <label for="username"> Username: </label>
-            <input type="text" id="username" v-model="username"></input>
-            <label for="username"> Username: </label>
-            <input type="text" id="password" v-model="password"></input>
+    <div class="signup">
+        <form @submit.prevent="">
+            <div>
+                <input></input>
+            </div>
+            <div>
+                <input></input>
+            </div>
+            <div>
+                <input></input>
+            </div>
+            <button type="submit">Sign Up</button>
         </form>
-
-        <div id="">
-            <button @click="SignUp" id="login"> Log In </button>
-            <button @click="SignUp" id="SignUp"> Sign Up </button>
-        </div>
-
     </div>
 </template>
 
@@ -21,17 +21,6 @@ import { ref } from 'vue';
 
 let username = ref('')
 let password = ref('')
-
-function LogIn() {
-    console.log("i shall not jump sid gupta")
-}
-
-function SignUp() {
-    console.log("can i jump sidhgupta")
-}
-
-function 
-
 
 </script>
 
