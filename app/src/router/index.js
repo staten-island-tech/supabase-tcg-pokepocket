796dfc51f076eaf2269c339b79dad3c988185ec4
SidhGupta <158204477+SidhGupta@users.noreply.github.com>
2025-04-10 11:55:31 -0400

    i

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,17 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import PackOpening from '../views/PackOpening.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../components/Login.vue'
+import Signup from '../components/Signup.vue'
+import Inventory from '../components/Inventory.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'PackOpening',
-    component: PackOpening,
-  },
-];
+  { path: '/login', component: Login },
+  { path: '/signup', component: Signup },
+  { path: '/inventory', component: Inventory, meta: { requiresAuth: true } },
+]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes,
-});
+})
 
-export default router;
+export default router
