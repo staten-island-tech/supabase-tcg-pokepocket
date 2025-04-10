3bfa16e424dc6b9c2427b28942bd8139f3b497ec
NotLeo27 <158211664+NotLeo27@users.noreply.github.com>
2025-04-10 12:15:40 -0400

    bombaclatt

--- app/src/supabase.js
+++ app/src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const accountURL = 'https://pvnqqrcshnnlnwozhmqs.supabase.co'
-const accountKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2bnFxcmNzaG5ubG53b3pobXFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxNzgwNDgsImV4cCI6MjA1ODc1NDA0OH0.OaL4_Xwy-4YtPS72VPbATIh8cfo9-3v23i-exl5j7sE'
+const accountURL = import.meta.env.VITE_SUPABASE_URL
+const accountKey = import.meta.env.VITE_SUPABASE_KEY
 const account = createClient(accountURL, accountKey)
 
 export default account
