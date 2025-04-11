c9e0937efe8d9574f9c723640d144858d75c04b9
SidhGupta <158204477+SidhGupta@users.noreply.github.com>
2025-04-11 12:02:47 -0400

    z

--- app/src/supabase.js
+++ app/src/supabase.js
@@ -5,12 +5,16 @@ import { createClient } from '@supabase/supabase-js';
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of f0a4f96 (n,)
 const supabaseUrl = process.env.VUE_APP_SUPABASE_URL;
 const supabaseKey = process.env.VUE_APP_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 =======
 const accountURL = import.meta.env.VITE_SUPABASE_URL
 const accountKey = import.meta.env.VITE_SUPABASE_KEY
+<<<<<<< HEAD
 =======
 const accountURL = 'https://pvnqqrcshnnlnwozhmqs.supabase.co'
 const accountKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2bnFxcmNzaG5ubG53b3pobXFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxNzgwNDgsImV4cCI6MjA1ODc1NDA0OH0.OaL4_Xwy-4YtPS72VPbATIh8cfo9-3v23i-exl5j7sE'
@@ -23,6 +27,8 @@ const accountKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSI
 const accountURL = 'https://pvnqqrcshnnlnwozhmqs.supabase.co'
 const accountKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2bnFxcmNzaG5ubG53b3pobXFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxNzgwNDgsImV4cCI6MjA1ODc1NDA0OH0.OaL4_Xwy-4YtPS72VPbATIh8cfo9-3v23i-exl5j7sE'
 >>>>>>> parent of 3bfa16e (bombaclatt)
+=======
+>>>>>>> parent of f0a4f96 (n,)
 const account = createClient(accountURL, accountKey)
 >>>>>>> 7aa9eb5b18ea9eafa84bee2ae8ec4e4cf252f221
 
