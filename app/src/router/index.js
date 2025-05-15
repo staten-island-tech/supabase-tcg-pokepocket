9fe7b9f92e1ce5191738eccab5153995930768ac
SidhGupta <158204477+SidhGupta@users.noreply.github.com>
2025-05-15 12:18:07 -0400

    dfs

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import SignupPage from '../views/SignupPage.vue';
 import Login from '../views/LoginPage.vue';
+import PackOpening from '../components/PackOpening.vue';
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/pack-opening',
+    name: 'PackOpening',
+    component: PackOpening
   },
 ];
 
--- app/src/views/LoginPage.vue
+++ app/src/views/LoginPage.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup>
-import Login from '@/components/LogIn.vue';
+import Login from '@/components/Login.vue';
 </script>
 
 <style scoped>
