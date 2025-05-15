1d7a6404d4c7c21270a11a78af67b4fc242e0bc1
NotLeo27 <158211664+NotLeo27@users.noreply.github.com>
2025-05-15 12:32:55 -0400

    e

--- app/src/views/LoginPage.vue
+++ app/src/views/LoginPage.vue
@@ -6,16 +6,8 @@
 </template>
 
 <script setup>
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Login from '@/components/Login.vue';
-=======
 import Login from '@/components/LogIn.vue'
 import TopBar from '@/components/TopBar.vue';
->>>>>>> d9ff4d8fdc590e7066a487d9a24ff42336e1ff3e
-=======
-import Login from '@/components/LogIn.vue';
->>>>>>> parent of 9fe7b9f (dfs)
 </script>
 
 <style scoped>
