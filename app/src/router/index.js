aae51ba98caf294a30030f9dbd8037943984f502
NotLeo27 <158211664+NotLeo27@users.noreply.github.com>
2025-05-16 11:42:45 -0400

    e

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import SignupPage from '../views/SignupPage.vue';
-
 import LoginPage from '../views/LoginPage.vue';
 
 const routes = [
