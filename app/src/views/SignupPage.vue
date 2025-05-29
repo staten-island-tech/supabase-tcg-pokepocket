bc4f70a94719b487f603f82111ef8e4a7abcaa05
NotLeo27 <158211664+NotLeo27@users.noreply.github.com>
2025-05-29 11:33:20 -0400

    e

--- app/src/views/SignupPage.vue
+++ app/src/views/SignupPage.vue
@@ -1,12 +1,10 @@
 <template>
     <div>
-        <TopBar /> 
         <SignUp />  
     </div>
 </template>
 
 <script setup>
-import TopBar from "../components/TopBar.vue"
 import SignUp from '@/components/SignUp.vue';
 </script>
 
