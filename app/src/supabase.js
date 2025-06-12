faef3f6e6a6ed4496b4a7f84c1aac3ff9d5790b5
NotLeo27 <158211664+NotLeo27@users.noreply.github.com>
2025-06-12 19:41:48 -0400

    ye

--- app/src/supabase.js
+++
@@ -1,9 +0,0 @@
-// src/supabase.js
-import { createClient } from '@supabase/supabase-js';
-
-const accountURL = import.meta.env.VITE_SUPABASE_URL
-const accountKey = import.meta.env.VITE_SUPABASE_KEY
-
-const account = createClient(accountURL, accountKey)
-
-export default supabase;
