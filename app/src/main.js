936c261457f646f13f1b78daaacd47b7e74f572d
NotLeo27 <158211664+NotLeo27@users.noreply.github.com>
2025-06-13 06:36:49 -0400

    a

--- app/src/main.js
+++ app/src/main.js
@@ -1,14 +1,25 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import router          from './router'
+import account         from './supabase'
+import App             from './App.vue'
+import { useAuthStore } from '@/stores/auth'
 
-import App from './App.vue'
-import router from './router'
+const pinia = createPinia()
+const app   = createApp(App)
 
-const app = createApp(App)
-
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
+
+const authStore = useAuthStore()
+
+account.auth.getSession().then(({ data: { session } }) => {
+  if (session?.user) authStore.setUser(session.user)
+})
+
+account.auth.onAuthStateChange((_e, session) => {
+  if (session?.user) authStore.setUser(session.user)
+  else authStore.clearUser()
+})
 
 app.mount('#app')
